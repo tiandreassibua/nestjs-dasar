cca727ad5f2a9792a9bcef1d5a68fa49760cb9c9
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2024-03-18 01:59:17 +0700

    HTTP Response

---
+++ src/user/user/user.controller.ts
@@ -0,0 +1,58 @@
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpRedirectResponse,
+  Param,
+  Post,
+  Query,
+  Redirect,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Controller('/api/users')
+export class UserController {
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      data: 'Hello JSON',
+    };
+  }
+
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      url: '/api/users/sample-response',
+      statusCode: 301,
+    };
+  }
+
+  @Get('/hello')
+  async sayHello(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string,
+  ): Promise<string> {
+    return `Hello ${firstName} ${lastName}`;
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string): string {
+    return `GET ${id}`;
+  }
+
+  @Post()
+  post(): string {
+    return 'POST';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'GET';
+  }
+}
