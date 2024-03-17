b9a77557172c4e64e280ef5864fdfadcef43b694
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2024-03-18 03:14:49 +0700

    unit test, create a test for user controller

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import * as httpMock from 'node-mocks-http';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -12,7 +13,19 @@ describe('UserController', () => {
     controller = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should can say hello', async () => {
+    const response = await controller.sayHello('Andreas', 'Sibua');
+    expect(response).toBe('Hello Andreas Sibua');
+  });
+
+  it('should can view template', async () => {
+    const response = httpMock.createResponse();
+    controller.viewHello('Andreas', response);
+
+    expect(response._getRenderView()).toBe('index.html');
+    expect(response._getRenderData()).toEqual({
+      name: "Andreas",
+      title: 'Template Engine',
+    });
   });
 });
