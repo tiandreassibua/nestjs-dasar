22352c79aeac130828160495d152e3a389942be8
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2024-03-18 02:29:55 +0700

    View using mustache view engine

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from "cookie-parser";
+import * as cookieParser from 'cookie-parser';
+import * as mustache from 'mustache-express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser("rahasia"))
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(cookieParser('RAHASIA'));
+  app.set('views', __dirname + '/../views');
+  app.set('view engine', 'html');
+  app.engine('html', mustache());
   await app.listen(3000);
 }
+
 bootstrap();
